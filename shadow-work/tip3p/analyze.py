c7a80aca5ddb4a5a59a4b3b81006814e11537f57
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-08-07 10:46:19 -0400

    Analyze all NetCDF files

--- shadow-work/tip3p/analyze.py
+++ shadow-work/tip3p/analyze.py
@@ -9,46 +9,60 @@ import netCDF4 as netcdf
 
 from simulate import nworksteps, timestep
 
-# Read work
-ncfile = netcdf.Dataset('work-DHFR-double.nc', 'r')
-work = ncfile.variables['work'][:,:]
-[nwork,nworkvals] = work.shape
+def analyze_ncfile(filename):
+    import os
+    name = os.path.splitext(filename)[0]
 
-if nwork == 0:
-    print('No work trajectories written yet.')
-    sys.exit(0)
+    # Read work
+    ncfile = netcdf.Dataset(filename, 'r')
+    work = ncfile.variables['work'][:,:]
+    [nwork,nworkvals] = work.shape
 
-# Plot results
-import matplotlib as mpl
-mpl.use('Agg')
-import matplotlib.pyplot as plt
+    if nwork == 0:
+        print('No work trajectories written yet.')
+        sys.exit(0)
 
-filename = 'tip3p-work.pdf'
-from matplotlib.backends.backend_pdf import PdfPages
-with PdfPages(filename) as pdf:
-    fig = plt.figure(figsize=(12,10))
-    t = np.arange(nworkvals) * nworksteps * timestep / unit.picoseconds
+    # Plot results
+    import matplotlib as mpl
+    mpl.use('Agg')
+    import matplotlib.pyplot as plt
 
-    plt.subplot(3,1,1)
-    plt.plot(t, work.T, '-')
-    plt.hold(True)
-    plt.plot(t, work.mean(0), '-', linewidth=3)
-    #plt.xlabel('trajectory length (ps)')
-    plt.ylabel('work (kT)')
+    filename = name + '.pdf'
+    from matplotlib.backends.backend_pdf import PdfPages
+    with PdfPages(filename) as pdf:
+        fig = plt.figure(figsize=(12,10))
+        t = np.arange(nworkvals) * nworksteps * timestep / unit.picoseconds
 
-    plt.subplot(3,1,2)
-    plt.plot(t, work.std(0), '-')
-    plt.xlabel('trajectory length (ps)')
-    plt.ylabel('work stddev (kT)')
+        plt.subplot(3,1,1)
+        plt.plot(t, work.T, '-')
+        plt.hold(True)
+        plt.plot(t, work.mean(0), '-', linewidth=3)
+        #plt.xlabel('trajectory length (ps)')
+        plt.ylabel('work (kT)')
 
-    plt.subplot(3,1,3)
-    Paccept = np.minimum(np.ones(work.shape), np.exp(-work)).mean(0)
-    dPaccept = np.exp(-work).std(0) / np.sqrt(nwork)
-    plt.gca().fill_between(t, Paccept-2*dPaccept, Paccept+2*dPaccept, facecolor='grey', interpolate=True, alpha=0.5)
-    plt.axis([0, t.max(), 0, 1])
-    plt.plot(t, Paccept, '-')
-    plt.xlabel('trajectory length (ps)')
-    plt.ylabel('acceptance probability')
+        plt.subplot(3,1,2)
+        plt.plot(t, work.std(0), '-')
+        plt.xlabel('trajectory length (ps)')
+        plt.ylabel('work stddev (kT)')
 
-    pdf.savefig()
-    plt.close()
+        plt.subplot(3,1,3)
+        Paccept = np.minimum(np.ones(work.shape), np.exp(-work)).mean(0)
+        dPaccept = np.exp(-work).std(0) / np.sqrt(nwork)
+        plt.gca().fill_between(t, Paccept-2*dPaccept, Paccept+2*dPaccept, facecolor='grey', interpolate=True, alpha=0.5)
+        plt.axis([0, t.max(), 0, 1])
+        plt.plot(t, Paccept, '-')
+        plt.xlabel('trajectory length (ps)')
+        plt.ylabel('acceptance probability')
+
+        pdf.savefig()
+        plt.close()
+
+if __name__ == '__main__':
+    import glob
+    filenames = glob.glob('*.nc')
+    for filename in filenames:
+        print(filename)
+        try:
+            analyze_ncfile(filename)
+        except Exception as e:
+            print(e)
